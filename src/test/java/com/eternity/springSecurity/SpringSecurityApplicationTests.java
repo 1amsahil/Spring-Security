08bcf5f3cd5eb2394a55184b68993a43ccd86183
1amsahil <sahil981156@gmail.com>
2025-04-22 12:30:11 +0530

    Spring Security Dependency ADDED

---
+++ src/test/java/com/eternity/springSecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eternity.springSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
