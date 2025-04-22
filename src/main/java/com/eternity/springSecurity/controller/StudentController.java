aa2c77c12baa089ba82fc21d02bad744db36322e
1amsahil <sahil981156@gmail.com>
2025-04-22 15:53:39 +0530

    Login Page config And CSRF Token

---
+++ src/main/java/com/eternity/springSecurity/controller/StudentController.java
@@ -0,0 +1,42 @@
+package com.eternity.springSecurity.controller;
+
+import com.eternity.springSecurity.model.Student;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    List<Student> students = new ArrayList<>(List.of(
+            new Student(1,"sahil","java"),
+            new Student(2,"Eternity","React"),
+            new Student(3,"Jack","JS")
+    ));
+
+    @GetMapping("list")
+    public List<Student> getList()
+    {
+        return students;
+    }
+
+    @PostMapping("addStudent")
+    public Student addStudent(@RequestBody Student st)
+    {
+        students.add(st);
+        return st;
+    }
+
+    @GetMapping("csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request)
+    {
+        return (CsrfToken)request.getAttribute("_csrf");
+    }
+
+}
