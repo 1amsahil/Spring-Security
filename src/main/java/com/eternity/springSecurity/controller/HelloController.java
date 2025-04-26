2c5ecad9f514a5a9cd6a988c05ccb877f12746de
1amsahil <sahil981156@gmail.com>
2025-04-26 15:24:43 +0530

    Adding Multiple User

--- src/main/java/com/eternity/springSecurity/controller/HelloController.java
+++ src/main/java/com/eternity/springSecurity/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping({"hello","/"})
     public String greeting(HttpServletRequest request)
     {
         return "Hello "+request.getSession().getId();
